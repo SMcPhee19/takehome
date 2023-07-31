178ac9049ea85fad12329efba87f039d09d07385
Stephen McPhee <120869196+SMcPhee19@users.noreply.github.com>
2023-07-31 13:53:29 -0600

    finish set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Customer.destroy_all
+Tea.destroy_all
+Subscription.destroy_all
+
 User.create!(first_name: 'Greg', last_name: 'Miller', email: 'ceosenior@kindafunny.com', address: '1234 Fake St')
 User.create!(first_name: 'Tim', last_name: 'Gettys', email: 'tim_mf_gettys@kindafunny.com', address: '123 Main Street')
 User.create!(first_name: 'Nick', last_name: 'Scarpino', email: 'producer_slash_seducer@kindafunny.com', address: '123 Maple Street')
@@ -16,3 +20,20 @@ User.create!(first_name: 'Barrett', last_name: 'Courtney', email: 'sadboybarrett
 User.create!(first_name: 'Mike', last_name: 'Howard', email: 'snowmikebike@kindafunny.com', address: '123 Birch Street')
 User.create!(first_name: 'Roger', last_name: 'Pecoraro', email: 'thelock@kindafunny.com', address: '123 Ash Street')
 User.create!(first_name: 'Greg', last_name: 'Gettys', email: 'coolgreg@kindafunny.com', address: '123 Walnut Street')
+
+Tea.create!(title: 'Earl Grey', description: 'A classic black tea with a citrusy twist', brew_temp: '200', brew_time: '3')
+Tea.create!(title: 'English Breakfast', description: 'A classic black tea', brew_temp: '184', brew_time: '5')
+Tea.create!(title: 'Black', description: 'A classic black tea', brew_temp: '197', brew_time: '1')
+Tea.create!(title: 'Masala Chai', description: 'A classic black tea with a spicy twist', brew_temp: '166', brew_time: '4')
+Tea.create!(title: 'Green', description: 'A classic green tea', brew_temp: '175', brew_time: '2')
+Tea.create!(title: 'Matcha', description: 'Green Tea Varietal grown in dark shaded areas', brew_temp: '178', brew_time: '5')
+Tea.create!(title: 'Sencha', description: 'Green Tea variety grown in sunlight', brew_temp: '154', brew_time: '4')
+Tea.create!(title: 'Oolong', description: 'A classic oolong tea', brew_temp: '200', brew_time: '3')
+Tea.create!(title: 'White', description: 'A classic white tea', brew_temp: '150', brew_time: '5')
+Tea.create!(title: "Pu'erh", description: "A classic pu'erh tea", brew_temp: '193', brew_time: '1')
+Tea.create!(title: 'Herbal', description: 'A classic herbal tea', brew_temp: '212', brew_time: '5')
+Tea.create!(title: 'Rooibos', description: 'A classic rooibos tea', brew_temp: '192', brew_time: '2')
+Tea.create!(title: 'Chamomile', description: 'A classic chamomile tea', brew_temp: '183', brew_time: '4')
+Tea.create!(title: 'Peppermint', description: 'A classic peppermint tea', brew_temp: '187', brew_time: '4')
+Tea.create!(title: 'Chrysanthemum', description: 'A classic chrysanthemum tea', brew_temp: '206', brew_time: '5')
+Tea.create!(title: 'Hibiscus', description: 'A classic hibiscus tea', brew_temp: '174', brew_time: '2')
--- spec/factories/tea.rb
+++ spec/factories/tea.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     title { Faker::Tea.variety }
     description { Faker::Tea.type }
     brew_temp { Faker::Number.between(from: 150, to: 212) }
-    brew_time { Faker::Number.between(from: 1, to: 5) }
+    brew_time { Faker::Number.between(from: 1, to: 6) }
   end
 end
